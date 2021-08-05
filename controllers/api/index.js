19abdb54402bc2f2084d3708b7a6ebbd4791266c
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-08-04 20:52:54 -0400

    Functional server skeleton created

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+// =============================================
+//  API Routes
+// ---------------------------------------------
+// Handles serving of dynamic resources via
+// handlebars templates
+// =============================================
+const router = require("express").Router();
+
+router.get("/", (req, res) => {});
+
+module.exports = router;
