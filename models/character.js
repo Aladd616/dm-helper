f29ca5d4ee24a65210f1c4542ad6d53c9637aabd
Allen Ladd <ghost2130@gmail.com>
2021-08-07 10:31:08 -0400

    Update character.js

--- models/character.js
+++ models/character.js
@@ -1,72 +1,114 @@
-const {Model, DataTypes} = require('sequelize');
-const sequelize = require('');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Character extends Model {}
 
 Character.init(
-    {
-        id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-         },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-       character_name: {
-        type: DataTypes.STRING,
-      }, 
-      race: {
-        type: DataTypes.STRING,
-      },
-      class: {
-        type: DataTypes.STRING,
-      },
-      level: {
-        type: DataTypes.INTEGER,
-        validate: {
-          isNumeric: true,
-        },
-      alignment: {
-        type: DataTypes.STRING,
-          },
-      size: {
-            type: DataTypes.STRING,
-          },
-      type: {
-        type: DataTypes.STRING,
-          },
-      hitDie: {
-        type: DataTypes.STRING,
-        },
-       hp: {
-        type: DataTypes.INTEGER,
-            },
-       initiative: {
-        type: DataTypes.INTEGER,
-            },
-       speed: {
-         type: DataTypes.INTEGER,
-            },
-        space: {
-         type: DataTypes.INTEGER,
-             },
-        reach: {
-          type: DataTypes.INTEGER,
-               },
-        armorClass: {
-            type: DataTypes.INTEGER,
-                },
-        attacks: {
-            type: DataTypes.TEXT,
-                    },
-        fullattack: {
-            type: DataTypes.TEXT,
-                },
-        specialattack: {
-             type: DataTypes.TEXT,
-                     },
-    
-        
-    }
-   
-)
+    character_name: {
+      type: DataTypes.STRING,
+    },
+    race: {
+      type: DataTypes.STRING,
+    },
+    class: {
+      type: DataTypes.STRING,
+    },
+    level: {
+      type: DataTypes.STRING,
+    },
+    alignment: {
+      type: DataTypes.STRING,
+    },
+    size: {
+      type: DataTypes.STRING,
+    },
+    type: {
+      type: DataTypes.STRING,
+    },
+    hitDie: {
+      type: DataTypes.STRING,
+    },
+    hp: {
+      type: DataTypes.INTEGER,
+    },
+    initiative: {
+      type: DataTypes.INTEGER,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+    },
+    space: {
+      type: DataTypes.INTEGER,
+    },
+    reach: {
+      type: DataTypes.INTEGER,
+    },
+    armorClass: {
+      type: DataTypes.INTEGER,
+    },
+    attacks: {
+      type: DataTypes.TEXT,
+    },
+    fullattack: {
+      type: DataTypes.TEXT,
+    },
+    specialattack: {
+      type: DataTypes.TEXT,
+    },
+    specialqual: {
+      type: DataTypes.TEXT,
+    },
+    fort: {
+      type: DataTypes.INTEGER,
+    },
+    reflex: {
+      type: DataTypes.INTEGER,
+    },
+    will: {
+      type: DataTypes.INTEGER,
+    },
+    dex: {
+      type: DataTypes.INTEGER,
+    },
+    con: {
+      type: DataTypes.INTEGER,
+    },
+    int: {
+      type: DataTypes.INTEGER,
+    },
+    wis: {
+      type: DataTypes.INTEGER,
+    },
+    cha: {
+      type: DataTypes.INTEGER,
+    },
+    skills: {
+      type: DataTypes.TEXT,
+    },
+    feats: {
+      type: DataTypes.TEXT,
+    },
+    equipment: {
+      type: DataTypes.TEXT,
+    },
+    notes: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscore: true,
+    modelName: "character",
+  }
+);
+
+module.exports = Character;
