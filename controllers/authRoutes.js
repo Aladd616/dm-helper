b4e94bde73f1442017cbc2087472b827215823f8
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-08-07 13:46:34 -0400

    Basic authentication routes created

--- controllers/authRoutes.js
+++ controllers/authRoutes.js
@@ -4,8 +4,23 @@
 // Handles authentication-specific routing
 // =============================================
 const router = require('express').Router();
+const passport = require('passport');
+const authCheck = require('connect-ensure-login').ensureLoggedIn;
 
-// Gets homepage
-router.get('/', (req, res) => {});
+// DEVELOPMENT TESTING
+router.get('/', authCheck(), (req, res, next) => {
+  res.status(200).json({ message: 'You are logged in!' });
+});
+
+// Logs in a user using passport's email/password auth scheme
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/auth/',
+    failureRedirect: '/login',
+    failureMessage: true,
+  }),
+  (req, res, next) => {}
+);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,9 +4,16 @@
 // Handles serving of static resources via
 // handlebars templates
 // =============================================
-const router = require("express").Router();
+const router = require('express').Router();
 
 // Gets homepage
-router.get("/", (req, res) => {});
+router.get('/', (req, res) => {});
+
+// Gets the login page
+// TODO: Rewrite this to render a web page and not
+// just respond with JSON
+router.get('/login', (req, res) => {
+  res.status(200).json({ message: 'Login page goes here.' });
+});
 
 module.exports = router;
