f797ee3044d5adb409f92af9d5769b95ca06af9d
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-08-10 14:11:12 -0400

    created dice object for advanced rolls,  began advanced rolling function

--- controllers/api/diceRoutes.js
+++ controllers/api/diceRoutes.js
@@ -13,17 +13,32 @@ function roll(size) {
   return Math.floor(Math.random() * size) + 1;
 }
 
+// purges spaces and check for invalid characters in the advanced roller input
 function sanitizeFormula(str) {
-  // purge spaces and check for invalid characters
   formula = str.split(' ').join('');
-  console.log(formula);
   if (/([^d0-9\+-])/.test(formula)) {
     throw 'Invalid syntax: Formula must be in the following format: 3d10+1d6+5';
   }
 
-  return formula;
+  // deconstructs the formula based on + or -
+  deconForm = formula.split(/([-\+])/g);
+  return deconForm;
 }
 
+function processDice(dice) {
+  let temp, single, total; // Variables for various temporary data
+
+  // loops through the deconstructed formula, handling each piece based on contents
+  for (let i = 0; i < dice.formula.length; i++) {
+    switch (true) {
+      case /\+/.test(dice.formula[i]):
+        dice.string += ' + ';
+        break;
+      case /-/.test(dice.formula[i]):
+        dice.string += ' - ';
+    }
+  }
+}
 // ------------------------------------------------------------------------------------------------
 
 // ------------------------------------------------------------------------------------------------
@@ -37,10 +52,20 @@ router.get('/:die', async (req, res) => {
 
 // ------------------------------------------------------------------------------------------------
 // Processes an arbitrary formula in a format such as 3d10+1d6+5
+// using the route /api/dice
 // TODO: Make this code less awful
 router.post('/', async (req, res) => {
   try {
-    res.status(200).json(sanitizeFormula(req.body.input));
+    let diceObject = {
+      formula: sanitizeFormula(req.body.dice),
+      sum: 0,
+      string: '',
+    };
+
+    diceObject = processDice(diceObject);
+
+    // Finalize output
+    res.status(200).json(diceObject);
   } catch (err) {
     res.status(400).json(err);
   }
