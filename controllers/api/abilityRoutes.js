1d161aecd1a4602be68ad5be4611714c4e82f264
Allen Ladd <ghost2130@gmail.com>
2021-08-09 21:20:39 -0400

    Update abilityRoutes.js

--- controllers/api/abilityRoutes.js
+++ controllers/api/abilityRoutes.js
@@ -24,7 +24,9 @@ router.get('/', async (req, res) => {
 // Gets one ability by its id
 router.get('/:id', async (req, res) => {
   try {
-    const abilityData = await Ability.findone();
+    const abilityData = await Ability.findOne({
+      where: { id: req.params.id },
+    });
 
     res.status(200).json(abilityData);
   } catch (err) {
@@ -32,5 +34,48 @@ router.get('/:id', async (req, res) => {
   }
 });
 // ------------------------------------------------------------------------------------------------
+router.post('/', async (req, res) => {
+  // create a new ability
 
+  try {
+    const abilityData = await Ability.create(req.body);
+    res.status(200).json(abilityData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+// ------------------------------------------------------------------------------------------------
+router.put('/:id', async (req, res) => {
+  try {
+    const ability = await Ability.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(ability);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  res.json(ability);
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete an ability by its `id`
+  try {
+    const abilityData = await Ability.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!abilityData) {
+      res.status(404).json({ message: 'No ability found with this id!' });
+      return;
+    }
+
+    res.status(200).json(abilityData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
