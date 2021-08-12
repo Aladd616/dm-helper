aa60a9d25bfdc9889d47337b63734bb86d96d100
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-08-12 14:16:40 -0400

    Added client side dice rolling script

---
+++ public/js/dice.js
@@ -0,0 +1,88 @@
+// ====================================================
+// Dice
+// ----------------------------------------------------
+// Handles dice rolling functionality
+// ====================================================
+const dice = (function () {
+  const rollHistory = $('#rollHistory');
+  const diceBar = $('#dice-bar');
+
+  // Prints a string to a list item in the roll history box
+  function printHistory(str) {
+    rollHistory.append($(`<li class="history-item">${str}</li>`));
+    limitList();
+  }
+
+  // Prunes old entries to the roll history
+  function limitList() {
+    let list = rollHistory.children();
+    if (list.length > 10) {
+      rollHistory.children().first().remove();
+    }
+  }
+  // Handles a click of a dice image
+  async function quickDice(event) {
+    event.preventDefault();
+    let size = $(event.target).parents('.quick-dice-box').attr('data-dietype');
+    let apiURL = `/api/dice/${size}`;
+
+    const result = await fetch(apiURL, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    $(this).siblings().html(result);
+
+    printHistory(
+      `<span class="quick-dice-result">Quick Dice:</span> 1d${size} = ${result}`
+    );
+  }
+
+  // Grabs a formula from the dice bar
+  function getFormula() {
+    let formula = diceBar.val();
+    console.log(formula);
+    diceBar.val('').change();
+
+    return formula;
+  }
+
+  // Handles an advanced dice roll, submitting the formula to the api
+  async function advDice(event) {
+    event.preventDefault();
+    formula = getFormula();
+
+    if (!formula) {
+      return null;
+    }
+
+    const response = await fetch('/dice', {
+      method: 'POST',
+      body: JSON.stringify({
+        dice: formula,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      printHistory(`${response}`);
+    } else {
+      printHistory(`Something went wrong with the server. Error: ${response}`);
+    }
+  }
+
+  // Sets up event handlers
+  function init() {
+    $('.quick-dice-button').on('click', quickDice);
+    $('#roll-button').on('click', advDice);
+  }
+
+  return {
+    init: init,
+  };
+})();
+
+// Load dice module when document is ready
+$(document).ready(function () {
+  dice.init();
+});
