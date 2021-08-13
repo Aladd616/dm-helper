5b1a9544e9ecf6057f58a1ea32f1a3ed0ab9f038
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-08-13 13:53:30 -0400

    Added signup route, signup link

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,24 +11,30 @@ const authCheck = require('connect-ensure-login').ensureLoggedIn;
 // Gets homepage
 router.get('/', authCheck('/login'), async (req, res) => {
   res.render('home', { user: req.user });
-  await console.log(req.user);
 });
 // ------------------------------------------------------------------------------------------------
 
 // ------------------------------------------------------------------------------------------------
 // Gets the login page
 router.get('/login', async (req, res) => {
-  console.log('User attempting login');
   if (req.query.error) {
-    console.log('Error present');
     res.render('login', { error: 'Unknown username or password combination.' });
   }
-  if (req.user) {
-    console.log(req.user);
+  if (req.session.loggedIn) {
     res.redirect('/');
   } else {
-    console.log('normal log in page');
     res.render('login');
+  }
+});
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// Gets the login page
+router.get('/signup', async (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+  } else {
+    res.render('signup');
   }
 });
 // ------------------------------------------------------------------------------------------------
